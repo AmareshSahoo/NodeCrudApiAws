c602c89b0d2bcc41ba809f5f0765cd42c7d7a549
Amaresh Sahoo <amaresh.sahoo@invincix.com>
2020-12-30 10:55:55 +0530

    change app

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ app.use(bodyParser.json());
 app.use(cors({ origin: "http://localhost:4200" }));
 
 
-app.use("/", (req, res) => { res.status(200).send("WELCOME TO AWS NODE API") });
+router.get("/", (req, res) => { res.status(200).send("WELCOME TO AWS NODE API By AMARESH") });
 
 app.use("/employees", employeeController);
 
-app.use('/health', (req, res) => { res.status(200).send("Status: OK") })
+router.get('/health', (req, res) => { res.status(200).send("Status: OK") })
 
 app.listen(process.env.PORT || 5000, () =>
     console.log("Server started at port : 5000")
